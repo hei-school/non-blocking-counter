be8aac556efc78471563502094810c5fdbb4698e
NyAndoMayah <hei.mayah.3@gmail.com>
2023-03-30 10:36:11 +0300

    feat: code

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
+
 const Counter = ({ from, to }) => {
   const consecutiveNumbers = [...Array(to - from).keys()].map((i) => i + from);
+  const eachPart = 100;
+  const firstPart = consecutiveNumbers.slice(start, start + eachPart);
+  const divide = consecutiveNumbers.length / eachPart;
+  const newConsecutiveNumbers = [];
+  useEffect(() => {
+    for (let j = 0; j < divide; j++) {
+      for (let i = 0; i < firstPart.length; i++) {
+        newConsecutiveNumbers.push(firstPart[i]);
+      }
+    }
+  }, newConsecutiveNumbers);
   return (
     <div>
-      {consecutiveNumbers.map((n) => (
+      {newConsecutiveNumbers.map((n) => (
         <p key={n}>{n}</p>
       ))}
     </div>
