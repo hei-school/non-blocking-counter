6fc4a50e460c127870286fa4c4873408466dbe56
Jonas4884 <hei.jonatana@gmail.com>
2023-03-30 09:59:34 +0300

    fix: generate 100 number per request

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,20 @@
-const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
+const Counter = ({ from, to }) => {
+  const divideInto = 20000;
+  let iteration = 0;
+  const generateRows = (from, to) => {
+    for (let i = from; i < to; i += 100) {
+      const div = document.createElement("div");
+      for (let t = i; t < to; t += 100) {
+        div.appendChild(
+          document.createTextNode((i)));
+        iteration++;
+        if (iteration < divideInto)
+          setTimeout(generateRows, 0);
+      };
+    }
+  }
+  return (    
+     generateRows(from, to)
+   )
 }
-
 export default Counter;
