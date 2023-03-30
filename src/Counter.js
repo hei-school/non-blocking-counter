05c8d7206d30210badb5282f51b9f1c3237f0e72
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-03-30 10:12:02 +0300

    fix: fix counter component

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,23 @@
+import {useEffect, useState} from "react";
+
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+  const batchSize = 100;
+  const [consecutiveNumbers, setConsecutiveNumbers] = useState([...Array(batchSize).keys()].map(i => i + from));
+  const [maxPrinted, setMaxPrinted] = useState(from + batchSize);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (maxPrinted >= to) {
+        clearInterval(interval);
+      } else {
+        const nextBatch = [...Array(batchSize).keys()].map(i => i + maxPrinted);
+        setConsecutiveNumbers([...consecutiveNumbers, ...nextBatch]);
+        setMaxPrinted(maxPrinted + batchSize);
+      }
+    }, 100);
+    return () => clearInterval(interval);
+  }, [consecutiveNumbers, maxPrinted, to]);
+
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
