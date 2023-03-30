de660b8e8d47619b47e3017b8cc19a78500bd342
J-Roussel <98948957+J-Roussel@users.noreply.github.com>
2023-03-30 10:31:45 +0300

    fix: counter rendering for large number ranges

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react";
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+  const [consecutiveNumbers, setConsecutiveNumbers] = useState([]);
+  const [maxPrinted, setMaxPrinted] = useState(from - 1);
+
+  useEffect(() => {
+    let cancelled = false;
+
+    const updateConsecutiveNumbers = () => {
+      const newMaxPrinted = Math.min(maxPrinted + 100, to);
+      const newConsecutiveNumbers = [...Array(newMaxPrinted - from + 1).keys()].map(i => i + from);
+      if (!cancelled) {
+        setConsecutiveNumbers(prev => [...prev, ...newConsecutiveNumbers.slice(prev.length)]);
+        setMaxPrinted(newMaxPrinted);
+      }
+      if (newMaxPrinted < to) {
+        requestAnimationFrame(updateConsecutiveNumbers);
+      }
+    };
+
+    requestAnimationFrame(updateConsecutiveNumbers);
+
+    return () => {
+      cancelled = true;
+    };
+  }, [from, to]);
+  
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
