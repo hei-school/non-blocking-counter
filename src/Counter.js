b0a8ee9d7f9417bba9aed79eee2963840b9a39d2
Rakotonirinaonjaniaina <hei.onjaniaina@gmail.com>
2023-03-30 09:44:27 +0300

    update counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,27 @@
-const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>
-}
-
-export default Counter;
+const Counter = ({ from, to }) => {
+  const [maxPrinted, setMaxPrinted] = React.useState(from);
+  const consecutiveNumbers = [];
+  
+  for (let i = maxPrinted; i < to && i < maxPrinted + 100; i++) {
+  consecutiveNumbers.push(i);
+  }
+  
+  React.useEffect(() => {
+  if (maxPrinted < to) {
+  const timeoutId = setTimeout(() => {
+  setMaxPrinted(maxPrinted + 100);
+  }, 0);
+  return () => clearTimeout(timeoutId);
+  }
+  }, [maxPrinted, to]);
+  
+  return (
+  <div>
+  {consecutiveNumbers.map((n) => (
+  <p key={n}>{n}</p>
+  ))}
+  </div>
+  );
+  };
+  
+  export default Counter;
