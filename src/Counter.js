ff41ea1eff8f779954477ed47c3eb02ea866b028
laurenrasoanaivo <hei.lauren.3@gmail.com>
2023-03-30 10:14:26 +0300

    todo: Q4

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,17 @@
+import React, { useState } from 'react';
+
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+  const [maxPrinted, setMaxPrinted] = useState(from - 1);
+
+  const consecutiveNumbers = [...Array(to-from).keys()]
+    .map(i => i + from)
+    .filter(n => n > maxPrinted)
+    .slice(0, 100);
+
+  if (consecutiveNumbers.length > 0) {
+    setMaxPrinted(consecutiveNumbers[consecutiveNumbers.length - 1]);
+  }
+
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
