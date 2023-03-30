0fa3fe89853bc7f261bd2324273cfe152a52ad2b
vohizy <hei.vohizy@gmail.com>
2023-03-30 09:37:57 +0300

    feat: make counter to no blocking

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Counter from './Counter';
+import Counter from "./Counter";
 
-const App = ({from1=0, to1=10, from2=200, to2=300}) =>
-  <div style={{ display: 'flex', flexDirection: 'row'}}>
-    <Counter from={from1} to={to1}/>
-    <span style={{width: '20%'}}/>
-    <Counter from={from2} to={to2}/>
-  </div>;
+const App = ({ from1 = 0, to1 = 10000, from2 = 200, to2 = 20000 }) => (
+  <div style={{ display: "flex", flexDirection: "row" }}>
+    <Counter from={from1} to={to1} />
+    <span style={{ width: "20%" }} />
+    <Counter from={from2} to={to2} />
+  </div>
+);
 
 export default App;
