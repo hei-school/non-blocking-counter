5993d5e13961614ac53adc458a5ab2522dae9081
NiceGuyMe <loica.etudiant@gmail.com>
2023-03-30 09:43:48 +0300

    fix: big numbers

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,27 @@
-const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
-}
+import React, { useState, useEffect } from 'react';
+
+const Counter = ({ from, to }) => {
+  const [maxPrinted, setMaxPrinted] = useState(from - 1);
+
+  useEffect(() => {
+    if (maxPrinted < to) {
+      const timeoutId = setTimeout(() => {
+        setMaxPrinted(Math.min(maxPrinted + 100, to));
+      }, 100); // Delay of 100 milliseconds before showing the next batch
+      return () => clearTimeout(timeoutId);
+    }
+  }, [maxPrinted, to]);
+
+  const numbers = [];
+  for (let i = from; i <= maxPrinted; i++) {
+    numbers.push(i);
+  }
+
+  return (
+      <div>
+        {numbers.map(n => <p key={n}>{n}</p>)}
+      </div>
+  );
+};
 
 export default Counter;
