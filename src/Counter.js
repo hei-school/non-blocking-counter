637988bc0e495c9b79c8189ed7c176b2cb5cd3d7
tahiry-dev <tahirycontact@gmail.com>
2023-03-30 10:22:30 +0300

    fix: make the counter code non-blocking

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,9 @@
-const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
-}
-
+import React, {useState, useEffect} from 'react';
+const Counter = ({ from, to }) => {
+  const batchSize = 100, [current, setCurrent] = useState(from - 1);
+  useEffect(() => {
+    if (current < to) setTimeout(() => setCurrent(current + batchSize), 0)},
+      [current]);
+  return <div>{Array(batchSize).fill().map((_, i) => current + i + 1 <= to && <p key={current + i + 1}>{current + i + 1}</p>)}</div>;
+};
 export default Counter;
