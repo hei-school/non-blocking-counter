e4743593b225a2503f8e0e2f898d1a11b92fdb24
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2023-03-30 10:13:27 +0300

    feat : Modify couter.js to make the second test work.

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,23 @@
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
-}
+  const MAX_NUMBERS = 1000;
+  const totalNumbers = to - from;
+  const numChunks = Math.ceil(totalNumbers / MAX_NUMBERS);
+  const chunks = Array.from({ length: numChunks }, (_, i) => ({
+    start: from + i * MAX_NUMBERS,
+    end: Math.min(from + (i + 1) * MAX_NUMBERS, to),
+  }));
+  
+  const chunkElements = chunks.map((chunk, index) => {
+    const { start, end } = chunk;
+    const consecutiveNumbers = Array(end - start).fill().map((_, i) => start + i);
+    return (
+      <div key={index}>
+        {consecutiveNumbers.map(n => <p key={n}>{n}</p>)}
+      </div>
+    );
+  });
+
+  return <div>{chunkElements}</div>;
+};
 
 export default Counter;
