cba790bdb22ef9bb65bc7a17cec50cbd06f0183d
Sanmandresy <98638893+Sanmandresy@users.noreply.github.com>
2023-03-30 09:54:59 +0300

    fix: count by batch of 100

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,25 @@
-const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
-}
+import { useState, useEffect } from 'react';
+
+const Counter = ({ from, to }) => {
+  const [batch, setBatch] = useState(1);
+  const consecutiveNumbers = [...Array(to - from).keys()].map((i) => i + from);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBatch((prevBatch) => prevBatch + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  
+  const start = (batch - 1) * 100;
+  const end = batch * 100;
+
+  return (
+    <div>
+      {consecutiveNumbers.slice(start, end).map((n) => (
+        <p key={n}>{n}</p>
+      ))}
+    </div>
+  );
+};
 
 export default Counter;
