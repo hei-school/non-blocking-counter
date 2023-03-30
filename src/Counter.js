26253000feef379a1a51a2d876cc3dea92b708df
Aina Rass <hei.aina.59@gmail.com>
2023-03-30 09:57:18 +0100

    fix: counte.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react';
+
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
-}
+  const [numbers, setNumbers] = useState([]);
+  useEffect(() => {
+    let current = from;
+    const interval = setInterval(() => {
+      if (current >= to) {
+        clearInterval(interval);
+        return;
+      }
+      const end = Math.min(current + 100, to);
+      setNumbers(prevNumbers => [...prevNumbers, ...[...Array(end - current).keys()].map(i => i + current)]);
+      current = end;
+    }, 10);
+    return () => clearInterval(interval);
+  }, [from, to]);
+  return <div>{numbers.map(n => <p key={n}>{n}</p>)}</div>;
+};
 
 export default Counter;
