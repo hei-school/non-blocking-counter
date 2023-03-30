b88f8eda7ccf84a0deb169d4e551c4c12fb0bf70
Michellah <hei.michellah@gmail.com>
2023-03-30 09:33:47 +0300

    fix: memory problems

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,12 @@
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+  const batchSize = 100;
+  const consecutiveNumbers = [];
+
+  for(let i = from; i<= to; i +=batchSize){
+    const batchEnd = Math.min(i + batchSize -1, to);
+    const batchNumbers =  [...Array(batchEnd - i + 1).keys()].map(j => j + i);
+    consecutiveNumbers.push(...batchNumbers);
+  }
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
