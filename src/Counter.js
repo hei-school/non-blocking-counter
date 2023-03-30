f355369d70970fb64e4cce379df8c05d8a0dc2ac
Mihary Joel <miharyjoel@gmail.com>
2023-03-30 09:36:02 +0300

    fix: add return semicolons

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,12 @@
-const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>
-}
+const Counter = ({ from, to }) => {
+  const consecutiveNumbers = [...Array(to - from).keys()].map((i) => i + from);
+  return (
+    <div>
+      {consecutiveNumbers.map((n) => (
+        <p key={n}>{n}</p>
+      ))}
+    </div>
+  );
+};
 
 export default Counter;
