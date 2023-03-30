7ae7702ad18eed1bf3ff90523c92e28f5d5d7fb2
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2023-03-30 20:27:43 +0300

    feat: counter doesn't block rendering

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,24 @@
-const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>
-}
+import {useState} from "react";
 
+const generateArray = (length, base) => [...Array(length).keys()].map(i => i + base);
+
+const Counter = ({from, to}) => {
+    const chunkSize = 200;
+    const initialLength = (Math.min((to - from), chunkSize))
+    const [array, setArray] = useState(generateArray(initialLength, from));
+    let iteration = 0;
+    const divideInto = Math.round((to - from) / chunkSize);
+    setTimeout(function addToArray() {
+        setArray((prevState) => {
+            const lastValue = prevState[prevState.length - 1];
+            const length = Math.min((to - lastValue), chunkSize);
+            return prevState.concat(generateArray(length, lastValue + 1));
+        })
+        iteration++;
+        if (iteration < divideInto) {
+            setTimeout(addToArray, 0);
+        }
+    }, 0);
+    return <div>{array.map(n => <p key={n}>{n}</p>)}</div>;
+}
 export default Counter;
