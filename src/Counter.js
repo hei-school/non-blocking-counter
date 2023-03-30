0e325d375ecef20f788189fe86d1cc8a137ba938
RyanIaro <hei.ryan.3@gmail.com>
2023-03-30 10:32:43 +0300

    fix(Counter.js): rendering large numbers smoothly

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 const Counter = ({from, to}) => {
+  const [ maxPrinted, setMaxPrinted ] = useState(100);
   const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+  setTimeout(function divide(){
+    for (let i = 0; i < maxPrinted; i++) {
+      consecutiveNumbers;
+    }
+    if (maxPrinted < to-from) {
+      setTimeout(divide(), 16);
+    };
+  }, 0);
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
