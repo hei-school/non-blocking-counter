b26f2f38018da116023292f06cb671e1dcd0619c
Rojotiana Nomena <hei.nomena@gmail.com>
2023-03-30 10:32:29 +0300

    fix: counter implementation

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,26 @@
 const Counter = ({ from, to }) => {
   const consecutiveNumbers = [...Array(to - from).keys()].map((i) => i + from);
+  const rowCount = to - from;
+  const divideInto = rowCount % 100;
+  const first = 0;
+  const last = 100;
+  const chunkSize = rowCount / divideInto;
+  let Iteration = 0;
+
   return (
     <div>
-      {consecutiveNumbers.map((n) => (
-        <p key={n}>{n}</p>
-      ))}
+      {setTimeout(function generateElement() {
+        consecutiveNumbers.map((n, i) => {
+          if (i < last && i >= first) {
+            <p key={n}>{n}</p>;
+          }
+        });
+        iteration++;
+
+        if (iteration < divideInto) {
+          setTimeout(generateElement, 0);
+        }
+      }, 0)}
     </div>
   );
 };
