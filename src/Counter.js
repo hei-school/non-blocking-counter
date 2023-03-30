10e438ee8eca3108813f98e8f1d931b6dc43b607
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2023-03-30 09:58:28 +0300

    fix: Counter.js file

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react';
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+  const [consecutiveNumbers, setConsecutiveNumbers] = useState([]);
+
+  useEffect(() => {
+    const displayNumbers = (from, to) => {
+      const numbers = [...Array(to-from).keys()].map(i => i + from);
+      setConsecutiveNumbers(numbers);
+      if (to < to) {
+        setTimeout(() => {
+          displayNumbers(from + 100, to + 100);
+        }, 0);
+      }
+    };
+    displayNumbers(from, from + 100);
+  }, [from, to]);
+
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
-
 export default Counter;
