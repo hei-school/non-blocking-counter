997fd2ee1eacb185855a44d450addbe59c9ebf0e
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-30 09:34:54 +0300

    chore: can quickly display large counted numbers

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react";
+
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
+  const [numbers, setNumbers] = useState([]);
+
+  useEffect(() => {
+    const batchSize = 100;
+    const numberOfBatches = Math.ceil((to - from) / batchSize);
+    let currentBatch = 0;
+    const intervalId = setInterval(() => {
+      const start = from + currentBatch * batchSize;
+      const end = Math.min(to, start + batchSize);
+      const batchNumbers = [...Array(end - start).keys()].map((i) => i + start);
+      setNumbers((prevNumbers) => [...prevNumbers, ...batchNumbers]);
+      currentBatch++;
+      if (currentBatch === numberOfBatches) {
+        clearInterval(intervalId);
+      }
+    }, 100);
+    return () => clearInterval(intervalId);
+  }, [from, to]);
+
+  return <div>{numbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
 export default Counter;
