f688af06f8127c9280281cf2f3fb1204f11f5bb2
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-03-30 10:34:12 +0300

    feat(unfinished): long-running counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,20 @@
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+
+///Generating data for the table
+const divideInto = 100;                                       
+const chunkSize = to/divideInto;  
+let consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+let iteration = 0;  
+
+setTimeout(function generateRows(){
+  for(from ; from <= to+100; to++ ){
+   consecutiveNumbers = [...Array(chunkSize).keys()].map(i => i + from);
+  }
+  iteration++;
+  if (iteration < divideInto)
+    setTimeout(generateRows, 0);
+},0);
+
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
