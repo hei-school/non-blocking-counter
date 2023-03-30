c12091e7c5849e78db0ae608ef64d352375501b5
Onitsiky <hei.onitsiky@gmail.com>
2023-03-30 10:31:41 +0300

    fix: creating counts

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+  const [tempFrom, setTempFrom] = useState(from);
+  const [tempTo, setTempTo] = useState(from + 99);
+  let consecutiveNumbers;
+  useEffect(() => {
+    if(tempTo < to){
+      const timer = setTimeout(() => {
+        setTempFrom(tempTo+1);
+        setTempTo(tempFrom + 99);
+      }, 0);
+      return () => clearTimeout(timer);
+    }
+  }, [tempTo, to]);
+  consecutiveNumbers = [...Array(tempFrom-tempTo).keys()].map(i => i + tempFrom);
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
