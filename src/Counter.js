ea1f58658f17174ec09bd2c5ae176187da12b243
Iloniavo <hei.iloniavo@gmail.com>
2023-03-30 10:42:45 +0300

    refactor(counter): increase performance

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,36 @@
-const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
-}
+import { useState, useEffect } from 'react';
+
+const Counter = ({ from, to }) => {
+  const [currentBatch, setCurrentBatch] = useState([]);
+
+  useEffect(() => {
+    let current = from;
+    let batch = [];
+
+    const interval = setInterval(() => {
+      batch = [...batch, current];
+      current++;
+
+      if (batch.length === 100 || current > to) {
+        setCurrentBatch(batch);
+        batch = [];
+      }
+
+      if (current === to) {
+        clearInterval(interval);
+      }
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, [from, to]);
+
+  return (
+    <div>
+      {currentBatch.map((n) => (
+        <p key={n}>{n}</p>
+      ))}
+    </div>
+  );
+};
 
 export default Counter;
