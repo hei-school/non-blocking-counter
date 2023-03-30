836e0c861a7e08caea7874e68fcf537a971cc45f
Miaritiana <97837302+Miaritiana@users.noreply.github.com>
2023-03-30 09:28:50 +0300

    fix: pass the second test

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,9 @@
 const Counter = ({from, to}) => {
   const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
+  const maxPrinted = 100;
+  for(let i=0; i<maxPrinted; i++) {
+    return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
+  }
 }
 
 export default Counter;
