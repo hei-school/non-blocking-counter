7235a9afda67de329b5b18c1f3ec9257d33351e5
Ezra <87571240+Ezdev2@users.noreply.github.com>
2023-03-30 09:49:04 +0300

    fix: counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,18 @@
+import { useState, useEffect } from 'react';
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+  const [consecutiveNumbers, setNumbers] = useState([]);
+  useEffect(() => {
+    const size = 100;
+    const getNextBatch = async (start, end) => {
+      const batchNumbers = [...Array(end - start).keys()].map((i) => i + start);
+      setNumbers((prevNumbers) => [...prevNumbers, ...batchNumbers]);
+      if (end < to) {
+        await new Promise((resolve) => setTimeout(resolve, 0));
+        await getNextBatch(end, Math.min(end + size, to));
+      }
+    };
+    getNextBatch(from, Math.min(from + size, to));
+  }, [from, to]);
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
