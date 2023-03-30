092f2a4bd5c0fc42289124cab0cf9c7a7144f323
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-03-30 09:38:23 +0300

    fix: perform sequence of numbers per 100

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+
 const Counter = ({from, to}) => {
-  const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
+  let [consecutiveNumbers, setConsecutiveNumbers] = useState([]);
+  let handleCount = ()=>{
+    const allnumbers = to-from;
+    const looping = allnumbers/100;
+    let sequence = []
+    let iterationCount = 0;
+    setTimeout(function generateNumber(){
+      const base = iterationCount*100;
+      for (let i = from; i < from+100; i++){
+        sequence.push(i+base)
+      }
+      iterationCount++;
+      setConsecutiveNumbers([...sequence])
+      if (iterationCount<looping) {
+        setTimeout(generateNumber, 0)
+      }
+    }, 0)
+  }
+  useEffect(()=>{
+    if (from>=100) {
+      handleCount()
+    }
+    setConsecutiveNumbers([...Array(to-from).keys()].map(i => i + from))
+  }, [])
   return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
