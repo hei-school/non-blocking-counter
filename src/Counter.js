7efb87b3cf638c8c8fbeab9cbbec46087bac1c3a
Mihary Joel <miharyjoel@gmail.com>
2023-03-30 10:17:59 +0300

    fix: counter test

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react";
+
 const Counter = ({ from, to }) => {
-  const consecutiveNumbers = [...Array(to - from).keys()].map((i) => i + from);
+  const [maxPrinted, setMaxPrinted] = useState(from - 1); // max number printed so far
+  const [numbers, setNumbers] = useState([]); // array of numbers to display
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newMax = maxPrinted + 100;
+      if (newMax <= to) {
+        setMaxPrinted(newMax);
+        setNumbers((prevNumbers) => [
+          ...prevNumbers,
+          ...[...Array(100).keys()].map((i) => i + newMax - 100),
+        ]);
+      }
+    }, 0);
+    return () => clearInterval(intervalId);
+  }, [maxPrinted, to]);
+
   return (
     <div>
-      {consecutiveNumbers.map((n) => (
+      {numbers.map((n) => (
         <p key={n}>{n}</p>
       ))}
     </div>
